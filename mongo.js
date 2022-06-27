37b0c55b6fa5069b2f9eb033e88471ec9366176b
shreyas772 <84660989+shreyas772@users.noreply.github.com>
2022-06-27 08:25:42 +0530

    Firstbit assignment

---
+++ mongo.js
@@ -0,0 +1,13 @@
+// {
+//     name:'Think and grow rich';
+//     pages:'350'
+//     price:'250'
+
+
+// }
+
+// to create data in mongo we have three functions 
+
+// 1. insert   (note:- its deprecated, no longer use)
+// 2. insertOne (it will take single object)
+// 3. insertMany (it will take multiple-array)
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js'],
+  plugins: [
+    require('tw-elements/dist/plugin')
+  ],
+
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      'fbox':'F8F7F5',
+    },
+  },
+}
